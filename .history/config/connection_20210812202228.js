304ba0028cebc8ba4549ef9d5aa181c982c984c3
Amanda Hoshor <hoshor.manda@gmail.com>
2021-08-12 20:23:10 -0600

    confi

--- .history/config/connection_20210812202228.js
+++ .history/config/connection_20210812202228.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 // create connection to our db
-const sequelize = process.env.JAWSDB_URL
+const sequelize = process.env.envJAWSDB_URL
   ? new Sequelize(process.envJAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
   host: 'localhost',
